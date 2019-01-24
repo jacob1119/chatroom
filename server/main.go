c8bc255bfaec23cf6de2b7783192ca48edd70036
jacob <1135030879@qq.com>
2019-01-24 16:49:30 +0800

    demo chatroot donw

--- server/main.go
+++ server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
+	"time"
 )
 
 type Client struct {
@@ -52,10 +54,67 @@ func HandleConn(conn net.Conn) {
 	//message <- "{" + cli.Addr + "}" + cli.Name + ": login "
 	message <- MakeMsg(cli, "login")
 
+	// 提示我是谁
+	cli.C <- MakeMsg(cli, "I am here")
+
+	isQuit := make(chan bool) //对方是否主动退出
+	hasData := make(chan bool) // 判断对方在某段时间内是否有数据
+
+	// 新建协程，接收用户发送来的数据
+	go func() {
+		buf := make([]byte, 1024)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 { //对方断开，或者出现问题
+				isQuit <- true
+				fmt.Println("conn read err = ", err)
+				return
+			}
+
+			msg := string(buf[:n-1])
+
+			if len(msg) == 3 && msg == "who" {
+				// 遍历map 给当前用户发送所有成员
+				conn.Write([]byte("user list :\n"))
+
+				for _, tmp := range onlineMap {
+					msg = tmp.Addr + ":" + tmp.Name + "\n"
+					conn.Write([]byte(msg))
+				}
+
+			} else if len(msg) >= 8 && msg[:6] == "rename" {
+				// rename | mike
+				name := strings.Split(msg, "|")[1]
+				cli.Name = name
+				onlineMap[cliAddr] = cli
+				conn.Write([]byte("rename ok\n"))
+			} else {
+				// 转发此内容
+				message <- MakeMsg(cli, msg)
+			}
+
+			hasData <- true
+
+		}
+	}()
+
 	for {
+		// 通过select检测channel流动
+		select {
+		case <-isQuit:
+			delete(onlineMap, cliAddr) // 当前用户从map中移除
+			message <- MakeMsg(cli, "login out")
+			return
 
+		case <- hasData:
+
+		case <- time.After(10*time.Second): // 超时时间
+			delete(onlineMap, cliAddr) // 当前用户从map中移除
+			message <- MakeMsg(cli, "has been leave!!!  because time out!! please be more behavior")
+			return
+
+		}
 	}
-
 }
 
 func WriteMsgToClient(cli Client, conn net.Conn) {
